5f2193e4cd06b92a5017380b437213a5abe3a3db
QuanP <quan.p@homecredit.vn>
2018-06-13 15:40:25 +0700

    Add Icon for Home Screen

--- Source/app/src/main/java/vn/homecredit/hcvn/ui/home/HomeActivity.java
+++ Source/app/src/main/java/vn/homecredit/hcvn/ui/home/HomeActivity.java
@@ -31,6 +31,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import vn.homecredit.hcvn.R;
+import vn.homecredit.hcvn.ui.support.SupportFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -72,16 +73,6 @@ public class HomeActivity extends AppCompatActivity {
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
@@ -156,13 +147,19 @@ public class HomeActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position) {
+                case 2:
+                    return SupportFragment.newInstance("A", "B");
+                default:
+                    break;
+            }
             return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 5;
         }
     }
 }
