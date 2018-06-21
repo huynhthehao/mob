bfbf669b2d30b6b9f21b0b10f4c692fd4fff409a
QuanP <quan.p@homecredit.vn>
2018-06-21 13:54:56 +0700

    fix bug android 4

--- Source/app/src/main/java/vn/homecredit/hcvn/HCVNApp.java
+++ Source/app/src/main/java/vn/homecredit/hcvn/HCVNApp.java
@@ -2,6 +2,8 @@ package vn.homecredit.hcvn;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.widget.Toast;
 
 //import com.androidnetworking.AndroidNetworking;
@@ -82,5 +84,42 @@ public class HCVNApp extends Application implements HasActivityInjector {
                         AppLogger.d(String.format("%s", additionalData));
                     }
                 }).init();
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle bundle) {
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
     }
 }
