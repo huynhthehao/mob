7329c47f5175bc1a3fbc7f42cff90a035ac99bb8
QuanP <quan.p@homecredit.vn>
2018-07-03 11:05:59 +0700

    layout loan application back button

--- Source/app/src/main/java/vn/homecredit/hcvn/ui/acl/validation/AclValidationActivity.java
+++ Source/app/src/main/java/vn/homecredit/hcvn/ui/acl/validation/AclValidationActivity.java
@@ -11,8 +11,11 @@ package vn.homecredit.hcvn.ui.acl.validation;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import javax.inject.Inject;
 
@@ -49,5 +52,16 @@ public class AclValidationActivity extends BaseActivity<ActivityAclValidationBin
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acl_validation);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return  super.onSupportNavigateUp();
     }
 }
