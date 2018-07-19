e789bbf61cb46ee25808d3e022f5c722370bdeaf
HCG\sy.nguyeng <sy.ng@homecredit.vn>
2018-07-19 14:54:15 +0700

    open call dialog for customer service

--- Source/app/src/main/java/vn/homecredit/hcvn/ui/profile/ProfileActivity.java
+++ Source/app/src/main/java/vn/homecredit/hcvn/ui/profile/ProfileActivity.java
@@ -1,11 +1,25 @@
 package vn.homecredit.hcvn.ui.profile;
 
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Toast;
+
 import javax.inject.Inject;
 
 import vn.homecredit.hcvn.R;
 import vn.homecredit.hcvn.databinding.ActivityProfileBinding;
 import vn.homecredit.hcvn.ui.base.BaseActivity;
 import vn.homecredit.hcvn.BR;
+import vn.homecredit.hcvn.utils.AppUtils;
 
 public class ProfileActivity extends BaseActivity<ActivityProfileBinding, ProfileViewModel> {
     @Inject
@@ -24,5 +38,17 @@ public class ProfileActivity extends BaseActivity<ActivityProfileBinding, Profil
     @Override
     public ProfileViewModel getViewModel() {
         return profileViewModel;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getViewDataBinding().tvProfileUpdateMessage.setMovementMethod(LinkMovementMethod.getInstance());
+        profileViewModel.init();
+        profileViewModel.getModelCustomerServiceCall().observe(this, customerServicePhone ->{
+            if(!TextUtils.isEmpty(customerServicePhone)){
+                AppUtils.openDeviceCallDialog(this, customerServicePhone);
+            }
+        } );
     }
 }
--- Source/app/src/main/java/vn/homecredit/hcvn/ui/profile/ProfileViewModel.java
+++ Source/app/src/main/java/vn/homecredit/hcvn/ui/profile/ProfileViewModel.java
@@ -1,13 +1,63 @@
 package vn.homecredit.hcvn.ui.profile;
 
+import android.arch.lifecycle.MutableLiveData;
+import android.databinding.ObservableField;
+import android.graphics.Typeface;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.view.View;
+
 import javax.inject.Inject;
 
+import vn.homecredit.hcvn.R;
+import vn.homecredit.hcvn.service.ResourceService;
 import vn.homecredit.hcvn.ui.base.BaseViewModel;
 import vn.homecredit.hcvn.utils.rx.SchedulerProvider;
 
 public class ProfileViewModel extends BaseViewModel {
+    private final ResourceService resourceService;
+    private ObservableField<SpannableString> profileUpdateMessage = new ObservableField<>();
+    private MutableLiveData<String> modelCustomerServiceCall = new MutableLiveData<>();
+    String customerServicePhone = "";
+
     @Inject
-    public ProfileViewModel(SchedulerProvider schedulerProvider) {
+    public ProfileViewModel(SchedulerProvider schedulerProvider, ResourceService resourceService) {
         super(schedulerProvider);
+        this.resourceService = resourceService;
+    }
+
+    @Override
+    public void init() {
+        customerServicePhone = resourceService.getStringById(R.string.customer_service_phone);
+        String profileMessage = resourceService.getStringById(R.string.update_profile_detail_message) + " " + customerServicePhone;
+        final SpannableString spannableString = new SpannableString(profileMessage);
+        spannableString.setSpan(clickableSpan, profileMessage.length() - customerServicePhone.length() - 1,
+                profileMessage.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        profileUpdateMessage.set(spannableString);
+    }
+
+    public ObservableField<SpannableString> getProfileUpdateMessage() {
+        return profileUpdateMessage;
+    }
+
+    final ClickableSpan clickableSpan = new ClickableSpan() {
+        @Override
+        public void onClick(final View view) {
+            modelCustomerServiceCall.setValue(customerServicePhone);
+        }
+
+        @Override
+        public void updateDrawState(TextPaint ds) {
+            super.updateDrawState(ds);
+            ds.setColor(resourceService.getColorById(R.color.primary_red));
+            ds.setUnderlineText(false);
+            ds.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
+        }
+    };
+
+    public MutableLiveData<String> getModelCustomerServiceCall() {
+        return modelCustomerServiceCall;
     }
 }
