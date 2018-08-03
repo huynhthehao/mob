d22dc263cebf46823a27c49b9465011f4a451b77
Nam Nguyen Trung (VN) <nam.nt6@homecredit.vn>
2018-08-03 13:02:15 +0700

    init payment history

---
+++ Source/app/src/main/java/vn/homecredit/hcvn/ui/contract/paymentHistory/PaymentHistoryActivity.java
@@ -0,0 +1,15 @@
+package vn.homecredit.hcvn.ui.contract.paymentHistory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.homecredit.hcvn.R;
+
+public class PaymentHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_history);
+    }
+}
