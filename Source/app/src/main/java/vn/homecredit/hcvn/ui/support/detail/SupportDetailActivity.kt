40030629383357b625d8e3a681ceb9da412a3b04
Cuong Thai Duy (VN) <Cuong.ThaiD@hcg.homecredit.net>
2018-08-16 17:25:58 +0700

    Implement Support Detail

---
+++ Source/app/src/main/java/vn/homecredit/hcvn/ui/support/detail/SupportDetailActivity.kt
@@ -0,0 +1,13 @@
+package vn.homecredit.hcvn.ui.support.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import vn.homecredit.hcvn.R
+
+class SupportDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_support_detail)
+    }
+}
