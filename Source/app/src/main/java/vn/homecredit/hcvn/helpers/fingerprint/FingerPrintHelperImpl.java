c0d4cb94e95ea9dbcf48dc1ab374e60288b61241
Hien Nguyen <hien.nm@homecredit.vn>
2018-08-23 10:21:54 +0700

    Fix fingerprint issue on Samsung device

--- Source/app/src/main/java/vn/homecredit/hcvn/helpers/fingerprint/FingerPrintHelperImpl.java
+++ Source/app/src/main/java/vn/homecredit/hcvn/helpers/fingerprint/FingerPrintHelperImpl.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Build;
 
+import com.samsung.android.sdk.SsdkVendorCheck;
+import com.samsung.android.sdk.pass.Spass;
+import com.samsung.android.sdk.pass.SpassFingerprint;
+
 import javax.inject.Inject;
 
 import vn.homecredit.hcvn.utils.FingerPrintAuthValue;
@@ -18,9 +22,13 @@ public class FingerPrintHelperImpl implements FingerPrintHelper {
 
     @Override
     public FingerPrintAuthValue getFingerPrintAuthValue() {
+        if (SsdkVendorCheck.isSamsungDevice())
+            return getSamsungFingerPrintAuthValue();
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //Fingerprint API only available on from Android 6.0 (M)
             FingerprintManager fingerprintManager = (FingerprintManager) context.getSystemService(Context.FINGERPRINT_SERVICE);
+
             if (!fingerprintManager.isHardwareDetected()) {
                 // Device doesn't support fingerprint authentication
                 return FingerPrintAuthValue.NOT_SUPPORT;
@@ -32,6 +40,29 @@ public class FingerPrintHelperImpl implements FingerPrintHelper {
                 return FingerPrintAuthValue.SUPPORT_AND_ENABLED;
             }
         }
+        return FingerPrintAuthValue.NOT_SUPPORT;
+    }
+
+
+    private FingerPrintAuthValue getSamsungFingerPrintAuthValue() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                Spass spass = new Spass();
+                spass.initialize(context);
+                SpassFingerprint spassFingerprint = new SpassFingerprint(context);
+
+                if(!spass.isFeatureEnabled(Spass.DEVICE_FINGERPRINT))
+                    return FingerPrintAuthValue.NOT_SUPPORT;
+
+                if (!spassFingerprint.hasRegisteredFinger())
+                    return FingerPrintAuthValue.SUPPORT_BUT_NOT_ENABLE;
+
+            } catch (Exception e) {
+                return FingerPrintAuthValue.NOT_SUPPORT;
+            }
+            return FingerPrintAuthValue.SUPPORT_AND_ENABLED;
+        }
+
         return FingerPrintAuthValue.NOT_SUPPORT;
     }
 }
