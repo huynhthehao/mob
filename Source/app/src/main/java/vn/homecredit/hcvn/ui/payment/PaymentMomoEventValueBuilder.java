882e0fcdb3025a637366ab2c61181c512ab85ea6
sy.ng <sy.ng@homecredit.vn>
2018-09-07 16:42:30 +0700

    Remove extraData for momo

--- Source/app/src/main/java/vn/homecredit/hcvn/ui/payment/PaymentMomoEventValueBuilder.java
+++ Source/app/src/main/java/vn/homecredit/hcvn/ui/payment/PaymentMomoEventValueBuilder.java
@@ -72,14 +72,6 @@ public class PaymentMomoEventValueBuilder {
         eventValue.put(MoMoParameterNamePayment.DESCRIPTION, context.getString(R.string.momo_payment_description));
         eventValue.put(MoMoParameterNamePayment.REQUEST_TYPE, "payment");
         eventValue.put(MoMoParameterNamePayment.LANGUAGE, languageCode);
-        JSONObject objExtraData = new JSONObject();
-        try {
-            objExtraData.put("orderLabel", context.getString(R.string.contract_number));
-            objExtraData.put("orderId", contractNumber);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        eventValue.put(MoMoParameterNamePayment.EXTRA_DATA, objExtraData.toString());
         return eventValue;
     }
 }
