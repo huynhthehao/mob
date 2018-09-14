a5a2ffacea00a1b9b674e566bfef66cf3eea75ce
Hien Nguyen <hien.nm@homecredit.vn>
2018-09-14 14:46:21 +0700

    Adding TODO

--- Source/app/src/main/java/vn/homecredit/hcvn/ui/home/HomeExtraMothodsListener.java
+++ Source/app/src/main/java/vn/homecredit/hcvn/ui/home/HomeExtraMothodsListener.java
@@ -2,6 +2,7 @@ package vn.homecredit.hcvn.ui.home;
 
 import vn.homecredit.hcvn.data.model.api.contract.HcContract;
 
+// TODO: Need to refactor
 interface HomeExtraMothodsListener {
     void openDefaultScheduleDetails(HcContract contract);
 }
